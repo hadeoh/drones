289460568aac682823e2e4283c8b34616c719f1f
Usman Adio <usthmandanfodio@gmail.com>
2022-02-11 18:42:38 +0100

    Write controller and tests

--- src/main/java/com/usmanadio/drone/controllers/MedicationController.java
+++ src/main/java/com/usmanadio/drone/controllers/MedicationController.java
@@ -7,12 +7,10 @@ import com.usmanadio.drone.services.MedicationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -33,6 +31,18 @@ public class MedicationController {
             response = Response.class)
     public ResponseEntity<Response<Medication>> loadDroneWithMedication(@RequestBody @Valid MedicationDto medicationDto) {
         Response<Medication> response = medicationService.loadDroneWithMedication(medicationDto);
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @GetMapping("/{droneId}")
+    @ApiOperation(value = "Load a drone with medications",
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE,
+            response = Response.class)
+    public ResponseEntity<Response<Page<Medication>>> checkLoadedMedicationsForDrone(
+            @PathVariable("droneId") Long droneId,
+            @RequestParam(required = false, name = "pageSize", defaultValue = "50") Integer pageSize,
+            @RequestParam(required = false, name = "pageNumber", defaultValue = "1") Integer pageNumber) {
+        Response<Page<Medication>> response = medicationService.checkLoadedMedicationsForDrone(droneId, pageNumber, pageSize);
         return new ResponseEntity<>(response, response.getStatus());
     }
 }
